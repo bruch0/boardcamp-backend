9fa33819ec7c5904029ad8c88a3ef81334ac4e5a
bruch0 <lucas.bruch0@gmail.com>
2021-10-14 09:55:00 -0300

    added categories route fully functional

--- app.js
+++ app.js
@@ -14,9 +14,57 @@ const connectionData = {
 	database: 'boardcamp'
 }
 
-const connection = Pool(connectionData)
+const connection = new Pool(connectionData)
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post('/categories', (req, res) => {
+	const newCategorie = req.body;
+
+	const objectRules = Joi.object({
+		name: Joi.string()
+			.required()
+	})
+
+	const marketRules = Joi.object({
+		name: Joi.string()
+			.min(1)
+			.required()
+	})
+
+	const objectHasRequiredProperties = objectRules.validate(newCategorie);
+
+	if (!objectHasRequiredProperties.error) {
+		const cleanString = stripHtml(newCategorie.name).result.trim();
+		const objectFulfillRules = marketRules.validate({name: cleanString});
+		if (!objectFulfillRules.error) {
+			connection.query('SELECT * FROM categories WHERE name = $1', [cleanString])
+				.then((result) => {
+					const newCategorieAlreadyExists = result.rows;
+					if (!newCategorieAlreadyExists.length) {
+						connection.query('INSERT INTO categories (name) VALUES ($1)', [newCategorie.name])
+							.then(() => res.sendStatus(201))
+					}
+					else {
+						res.sendStatus(409);
+					}
+				});
+			
+		}
+		else {
+			res.status(400).send('A propriedade name deve ter no mínimo 1 caractere')
+		}
+	}
+	else {
+		res.status(400).send('O objeto deve conter a propriedade "name"');
+	}
+})
+
+app.get('/categories', (req, res) => {
+	connection.query('SELECT * FROM categories').then((response) => res.send(response.rows))
+})
+
+
+app.listen(4000);
