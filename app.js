c3382b06696ca8a24a29e6b115a378dab7e22329
bruch0 <lucas.bruch0@gmail.com>
2021-10-16 15:53:13 -0300

    added rentals route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import pg from 'pg'
 import { stripHtml } from "string-strip-html";
 import Joi from 'joi';
+import dayjs from 'dayjs'
 
 const { Pool } = pg
 
@@ -194,7 +195,9 @@ app.get('/games', async (req, res) => {
 	const name = req.query.name ? `WHERE games.name ILIKE '${req.query.name}%'` : '';
 	const offset = Number(req.query.offset) ? `OFFSET ${Number(req.query.offset)}` : '';
 	const limit = Number(req.query.limit) ? `LIMIT ${Number(req.query.limit)}` : '';
-	const query = `SELECT games.*, categories.name AS "categoryName" FROM games  JOIN categories ON games."categoryId" = categories.id ${name} ${limit} ${offset}  ;`
+	const order = req.query.order ? `ORDER BY ${req.query.order}` : '';
+	const descOrder = req.query.desc ? `DESC` : ''
+	const query = `SELECT games.*, categories.name AS "categoryName" FROM games  JOIN categories ON games."categoryId" = categories.id ${name} ${order} ${descOrder} ${limit} ${offset} ;`
 
 	try {
 		const response = await connection.query(query)
@@ -384,8 +387,10 @@ app.get('/customers', async (req, res) => {
 	const cpf = req.query.cpf ? `WHERE cpf ILIKE '${req.query.cpf}%'` : '';
 	const offset = Number(req.query.offset) ? `OFFSET ${Number(req.query.offset)}` : '';
 	const limit = Number(req.query.limit) ? `LIMIT ${Number(req.query.limit)}` : '';
+	const order = req.query.order ? `ORDER BY ${req.query.order}` : '';
+	const descOrder = req.query.desc ? `DESC` : ''
 
-	const query = `SELECT * FROM customers ${cpf} ${offset} ${limit};`
+	const query = `SELECT * FROM customers ${cpf} ${order} ${descOrder} ${offset} ${limit};`
 	try {
 		const response = await connection.query(query);
 		res.send(response.rows);
@@ -411,6 +416,171 @@ app.get('/customers/:customerId', async (req, res) => {
 	}
 })
 
+app.post('/rentals', async (req, res) => {
+	const newRent = req.body;
 
+	const marketRules = Joi.object({
+		customerId: Joi.number()
+			.required(),
+
+		gameId: Joi.number()
+			.required(),
+
+		daysRented: Joi.number()
+			.greater(1)
+			.required(),
+	});
+
+	const objectFulfillRules = marketRules.validate(newRent);
+
+		if (!objectFulfillRules.error) {
+			try {
+				const result = await connection.query('SELECT * FROM customers WHERE id = $1', [newRent.customerId]);
+				const customerExists = result.rows;
+				if (customerExists.length) {
+					try {
+						const result = await connection.query('SELECT * FROM games WHERE id = $1', [newRent.gameId]);
+						const gameExists = result.rows;
+						if (gameExists.length && gameExists[0].stockTotal > 0) {
+							const pricePerDay = gameExists[0].pricePerDay;
+							try {
+								const time = Date.now();
+								const newRentObj = {
+									...newRent,
+									rentDate: dayjs(time).format('YYYY-MM-DD'),
+									originalPrice: newRent.daysRented * pricePerDay
+								}
+								
+								await connection.query('INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)',
+								[
+									newRentObj.customerId,
+									newRentObj.gameId,
+									newRentObj.daysRented,
+									newRentObj.rentDate,
+									newRentObj.originalPrice,
+									null,
+									null,
+								]);
+								res.sendStatus(201);
+							}
+							catch {
+								res.sendStatus(503);
+							}
+						}	
+						else {
+							res.sendStatus(400);
+						}
+					}
+					catch {
+						res.sendStatus(400);
+					}
+				}
+				else {
+					res.sendStatus(400);
+				}
+			}
+			catch {
+				res.sendStatus(503);
+			}
+			
+		}
+		else {
+			res.status(400).send('A propriedade "name" deve ter no mínimo 1 caractere, "phone" deve ter entre 10 e 11 caracteres, "cpf" deve ter 11 caracteres e "birthday" deve ser no formato yyyy-mm-dd')
+		}
+
+})
+
+app.post('/rentals/:rentId/return', async (req, res) => {
+	const rentId = Number(req.params.rentId);
+	try {
+		const result = await connection.query('SELECT * FROM rentals WHERE id = $1', [rentId]);
+		const date = dayjs(Date.now()).format('YYYY-MM-DD');
+		const fee = (result.rows[0].originalPrice / result.rows[0].daysRented) * dayjs(result.rows[0].rentDate).diff(dayjs(), 'day');
+		if (result.rows.length && !result.rows[0].returnDate) {
+			try {
+				await connection.query('UPDATE rentals SET "returnDate" = $2, "delayFee" = $3 WHERE id = $1;', [result.rows[0].id, date, fee])
+				res.sendStatus(200);
+			}
+			catch {
+				res.sendStatus(503);
+			}
+		}
+		else {
+			if (!result.rows.length) {
+				res.sendStatus(404)
+			}
+			else {
+				res.sendStatus(400)
+			}
+		}
+	}
+	catch {
+		res.sendStatus(503);
+	}
+})
+
+app.delete('/rentals/:rentId', async (req, res) => {
+	const rentId = Number(req.params.rentId);
+	try {
+		const result = await connection.query('SELECT * FROM rentals WHERE id = $1', [rentId]);
+		const rent = result.rows;
+		if (rent.length && !rent[0].returnDate) {
+			try {
+				await connection.query('DELETE FROM rentals WHERE id = $1;', [rentId])
+				res.sendStatus(200);
+			}
+			catch {
+				res.sendStatus(503);
+			}
+		}
+		else {
+			if (!rent.length) {
+				res.sendStatus(404)
+			}
+			else {
+				res.sendStatus(400)
+			}
+		}
+	}
+	catch {
+		res.sendStatus(503);
+	}
+})
+
+app.get('/rentals', async (req, res) => {
+	const customerId = req.query.customerId ? `WHERE "customerId" = ${req.query.customerId}` : '';
+	const gameId = req.query.gameId ? `WHERE "gameId" = ${req.query.gameId}` : '';
+	const offset = Number(req.query.offset) ? `OFFSET ${Number(req.query.offset)}` : '';
+	const limit = Number(req.query.limit) ? `LIMIT ${Number(req.query.limit)}` : '';
+	const order = req.query.order ? `ORDER BY ${req.query.order}` : '';
+	const descOrder = req.query.desc ? `DESC` : ''
+
+	try {
+		const result = await connection.query(`SELECT rentals.*, customers.name, games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON rentals."customerId" = customers."id" JOIN games ON rentals."gameId" = games.id JOIN categories ON games."categoryId" = categories.id ${customerId} ${gameId} ${order} ${descOrder} ${offset} ${limit};`);
+		result.rows.map((rent, index) => {
+			rent.customer = {
+				id: rent.customerId,
+				name: rent.name
+			};
+			rent.game = {
+				id: rent.gameId,
+				name: rent.gameName,
+				categoryId: rent.categoryId,
+				categoryName: rent.categoryName
+			};
+			delete rent.name;
+			delete rent.categoryId;
+			delete rent.categoryName;
+			delete rent.gameName;
+			if (index === result.rows.length - 1) {
+				res.send(result.rows);
+			}
+		})
+
+	}
+	catch {
+		res.sendStatus(503);
+	}
+})
 
 app.listen(4000);
